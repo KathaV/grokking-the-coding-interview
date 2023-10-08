ea84db15b3cc0b970a97e7b1851c8e310ab24094
KathaV <villanueva.katha@gmail.com>
2023-10-08 13:27:15 +1100

    Added Merge INtervals

---
+++ src/Problems/_3_MergeIntervals/MergeIntervals/Interval.java
@@ -0,0 +1,10 @@
+package Problems._3_MergeIntervals.MergeIntervals;
+
+public class Interval {
+    int start;
+    int end;
+    public Interval (int start, int end){
+        this.start = start;
+        this.end = end;
+    }
+}
---
+++ src/Problems/_3_MergeIntervals/MergeIntervals/MergeIntervals.java
@@ -0,0 +1,10 @@
+package Problems._3_MergeIntervals.MergeIntervals;
+import java.util.*;
+
+public class MergeIntervals {
+    public static List<Interval> merge(List<Interval> intervals){
+        List<Interval> mergedIntervals = new LinkedList<Interval>();
+
+        return mergedIntervals;
+    }
+}
